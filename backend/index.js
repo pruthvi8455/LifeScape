d2e9f8a80dc99bad34bcea0ca2f4a6af48a8100f
rohit <rohitdol21@gmail.com>
2024-01-16 15:32:43 +0530

    added an express server in the backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
